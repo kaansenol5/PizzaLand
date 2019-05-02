8c9da56b6b58ebbe11cbfaaecc5d103484efad49
kaan <moonman76@protonmail.com>
2019-05-02 18:37:49 +0300

    added command line launcher with zero dependencies

--- launcher.py
+++ launcher.py
@@ -5,77 +5,139 @@ import time
 import zipfile
 import subprocess
 import requests
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QApplication
+import configparser
+config = configparser.ConfigParser()
+config.read("launcherconfig.config")
+default=config.get("launcher","default-style")
+branch=config.get("launcher","default-branch")
 
-try:
-    shutil.rmtree("assets")
-    os.remove("PizzaLandGame.py")
-except Exception:
-    pass
-
-dlLink = "https://github.com/kaansenol5/PizzaLand/archive/release.zip"
+def install(pkg):
+    subprocess.call(["python3","-m","pip","install",pkg,"--user"])
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(751, 552)
-        self.val = 0
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(200, 70, 361, 121))
-        self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(220, 370, 301, 81))
-        self.pushButton.setObjectName("pushButton")
-        self.progressBar = QtWidgets.QProgressBar(Dialog)
-        self.progressBar.setGeometry(QtCore.QRect(240, 320, 251, 23))
-        self.progressBar.setProperty("value", self.val)
-        self.progressBar.setObjectName("progressBar")
-        self.pushButton.clicked.connect(self.downLoadGame)
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("PizzaLand", "PizzaLand"))
-        self.label.setText(_translate("PizzaLand",
-                                      "<html><head/><body><p><span style=\" font-size:26pt; font-weight:600;\">PizzaLand Launcher</span></p></body></html>"))
-        self.pushButton.setText(_translate("PizzaLand", "Play"))
-
-    def downLoadGame(self):
-        print("aaaaaaaa")
-        self.val = 25
-        self.progressBar.setProperty("value", self.val)
-        r = requests.get(dlLink)
-        self.val = 30
-        self.progressBar.setProperty("value", self.val)
-        with open("game.zip", "wb") as f:
-            f.write(r.content)
-            self.val = 50
-            self.progressBar.setProperty("value", self.val)
-        zip_ref = zipfile.ZipFile("game.zip", "r")
-        zip_ref.extractall("")
-        os.remove("game.zip")
-        shutil.move("PizzaLand-release/assets", "assets")
-        shutil.move("PizzaLand-release/PizzaLandGame.py", "PizzaLandGame.py")
-        os.remove("launcher.py")
-        shutil.move("PizzaLand-release/launcher.py", "launcher.py")
-        self.val = 100
-        self.progressBar.setProperty("value", self.val)
-        time.sleep(1)
-        try:
-            import pygame
-        except ImportError:
-            subprocess.call(["python3", "-m", "pip", "install", "pygame"])
-        os.system("python3 PizzaLandGame.py")
+class colors():
+    RED = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    app.processEvents()
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+cmdhelp = f""""{colors.RED}
+launch                     Launches the game
+set-default-style-cmd      sets the default launcher to command line
+set-default-style-gui      sets the default launcher to gui
+set-default-branch-release sets the default git branch to release
+set-default-branch-master  sets the default git branch to master
+help                       displays this message {colors.RESET}
+"""
+
+
+def downLoadGame():
+    try:
+        shutil.rmtree("assets")
+        os.remove("PizzaLandGame.py")
+    except Exception:
+        pass
+    dlLink = f"https://github.com/kaansenol5/PizzaLand/archive/{branch}.zip"
+    r = requests.get(dlLink)
+    with open("game.zip", "wb") as f:
+        f.write(r.content)
+    zip_ref = zipfile.ZipFile("game.zip", "r")
+    zip_ref.extractall("")
+    os.remove("game.zip")
+    shutil.move("PizzaLand-release/assets", "assets")
+    shutil.move("PizzaLand-release/PizzaLandGame.py", "PizzaLandGame.py")
+    os.remove("launcher.py")
+    shutil.move("PizzaLand-release/launcher.py", "launcher.py")
+    time.sleep(1)
+    try:
+        import pygame
+    except ImportError:
+        install("pygame")
+    os.system("python3 PizzaLandGame.py")
+
+def setdefault(default,thing):
+    config.set("launcher", thing, default)
+    with open("launcherconfig.config","wb") as f:
+        config.write(f)
+        
+def cmdLauncher():
+    print(f"{colors.CYAN} Welcome to the command-line pizzaland launcher!")
+    print('If you want to save this option, type "save" to the command line')
+    print("To display a list of comments, type help")
+    print("You are on your own")
+    cmdline=True
+    while cmdline:
+        cmd=input()
+        if cmd == "launch":
+            downLoadGame()
+        elif cmd == "set-default-style-gui":
+            setdefault("gui","default-style")
+        elif cmd == "set-default-style-cmd":
+            setdefault("cmd","default-style")
+        elif cmd == "set-default-branch-release":
+            setdefault("release","default-branch")
+        elif cmd == "set-default-branch-master":
+            setdefault("master","default-branch")
+        elif cmd == "help":
+            print(cmdhelp)
+        else:
+            print(cmdhelp)
+
+
+def guiLauncher():
+    class Ui_Dialog(object):
+        def setupUi(self, Dialog):
+            Dialog.setObjectName("Dialog")
+            Dialog.resize(751, 552)
+            self.val = 0
+            self.label = QtWidgets.QLabel(Dialog)
+            self.label.setGeometry(QtCore.QRect(200, 70, 361, 121))
+            self.label.setObjectName("label")
+            self.pushButton = QtWidgets.QPushButton(Dialog)
+            self.pushButton.setGeometry(QtCore.QRect(220, 370, 301, 81))
+            self.pushButton.setObjectName("pushButton")
+            self.pushButton.clicked.connect(downLoadGame())
+            self.retranslateUi(Dialog)
+            QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+        def retranslateUi(self, Dialog):
+            _translate = QtCore.QCoreApplication.translate
+            Dialog.setWindowTitle(_translate("PizzaLand", "PizzaLand"))
+            self.label.setText(_translate("PizzaLand",
+                                          "<html><head/><body><p><span style=\" font-size:26pt; font-weight:600;\">PizzaLand Launcher</span></p></body></html>"))
+            self.pushButton.setText(_translate("PizzaLand", "Play"))
+
+    if default == "gui":
+        if __name__ == '__main__':
+            app = QApplication(sys.argv)
+            app.processEvents()
+            MainWindow = QtWidgets.QMainWindow()
+            ui = Ui_Dialog()
+            ui.setupUi(MainWindow)
+            MainWindow.show()
+            sys.exit(app.exec_())
+
+
+if default == "gui":
+    try:
+        from PyQt5 import QtCore, QtWidgets
+        from PyQt5.QtWidgets import QApplication
+        guiLauncher()
+    except ImportError:
+        print("Failed to import PyQt5")
+        print("This can be fix by installing PyQt5.")
+        print("Do you want to install PyQt5 y/n")
+        print("If you choose no, you can use command line launcher")
+        mode=input()
+        if mode == "y":
+            install("PyQt5")
+        else:
+            cmdLauncher()
+
+else:
+    cmdLauncher()
+
